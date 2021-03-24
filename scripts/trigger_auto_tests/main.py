444185395b727be8309e1dcf2d7a90cfdffe5dd7
Kyrylo Maksymenko <saklar13@gmail.com>
2021-03-24 19:34:20 +0200

    use TC messages to show running tests

--- scripts/trigger_auto_tests/main.py
+++ scripts/trigger_auto_tests/main.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import click
 from dohq_teamcity import TeamCity
+from teamcity.messages import TeamcityServiceMessages
 
 from scripts.trigger_auto_tests.utils.helpers import (
     AutoTestsInfo,
@@ -22,37 +23,44 @@ def main(tc_user: str, tc_password: str):
     triggered_builds: dict[str, int] = {}
     tc = TeamCity(TC_URL, auth=(tc_user, tc_password))
     tests_info = AutoTestsInfo.get_current(tc)
+    tc_msg = TeamcityServiceMessages()
     if tests_info.re_run_builds:
         click.echo("Re run failed builds")
     else:
         click.echo("Run automated tests")
 
-    for shell_name in tests_info.supported_shells:
-        try:
-            build_id = _run_tests_for_shell(tc, shell_name, tests_info)
-            triggered_builds[shell_name] = build_id
-        except Exception as e:
-            errors.append(e)
-            click.echo(e, err=True)
+    tc_msg.testCount(len(tests_info.supported_shells))
+    with tc_msg.testSuite("Automation tests"):
+        for shell_name in tests_info.supported_shells:
+            try:
+                build_id = _run_tests_for_shell(tc, tc_msg, shell_name, tests_info)
+                triggered_builds[shell_name] = build_id
+            except Exception as e:
+                errors.append(e)
+                click.echo(e, err=True)
 
-    builds_statuses, new_errors = _wait_build_finish(tc, triggered_builds)
-    errors.extend(new_errors)
+        builds_statuses, new_errors = _wait_build_finish(tc, tc_msg, triggered_builds)
+        errors.extend(new_errors)
 
-    if errors:
-        raise Exception("There were errors running automation tests.")
+        if errors:
+            raise Exception("There were errors running automation tests.")
     return all(builds_statuses.values())
 
 
 def _run_tests_for_shell(
-    tc: TeamCity, shell_name: str, tests_info: AutoTestsInfo
+    tc: TeamCity,
+    tc_msg: TeamcityServiceMessages,
+    shell_name: str,
+    tests_info: AutoTestsInfo,
 ) -> Optional[int]:
     build_id = None
     if is_shell_uses_package(shell_name, tests_info):
         if tests_info.re_run_builds:
             if is_last_build_successful(tc, shell_name, tests_info):
-                click.echo(
+                tc_msg.testIgnored(
+                    shell_name,
                     f"{shell_name} last auto tests for this package and commit "
-                    f"id was successful, skip it"
+                    f"id was successful, skip it",
                 )
             else:
                 click.echo(f"{shell_name} Re run automation tests")
@@ -61,27 +69,35 @@ def _run_tests_for_shell(
             click.echo(f"{shell_name} Automation tests build triggering")
             build_id = trigger_auto_tests_build2(tc, shell_name, tests_info)
     else:
-        click.echo(f"{shell_name} is not uses package with this version, skipped tests")
+        tc_msg.testIgnored(
+            shell_name,
+            f"{shell_name} is not uses package with this version, skipped tests",
+        )
     return build_id
 
 
 def _wait_build_finish(
-    tc: TeamCity, triggered_builds: dict[str, int]
+    tc: TeamCity, tc_msg: TeamcityServiceMessages, triggered_builds: dict[str, int]
 ) -> tuple[dict[str, bool], list[Exception]]:
     builds_statuses = {}
     errors = []
+    start_time = time.time()
     while triggered_builds:
         time.sleep(BUILDS_CHECK_DELAY)
         for shell_name, build_id in triggered_builds.copy().items():
             try:
                 build = tc.builds.get(f"id:{build_id}")
                 if is_build_finished(build):
-                    click.echo(
-                        f"{shell_name} Automation tests is finished "
-                        f"with status {build.status}"
-                    )
-                    builds_statuses[shell_name] = is_build_success(build)
-                    triggered_builds.pop(shell_name)
+                    with tc_msg.test(shell_name, testDuration=time.time() - start_time):
+                        is_success = is_build_success(build)
+                        builds_statuses[shell_name] = is_success
+                        triggered_builds.pop(shell_name)
+                        if not is_success:
+                            tc_msg.testFailed(
+                                shell_name,
+                                f"{shell_name} Automation tests is finished"
+                                f" with status {build.status}",
+                            )
             except Exception as e:
                 errors.append(e)
                 click.echo(e, err=True)
