2a422dff9fad520bd6454744cee6690dbbc312b4
Kyrylo Maksymenko <saklar13@gmail.com>
2021-08-27 16:38:22 +0300

    update triggering auto tests

--- scripts/cli.py
+++ scripts/cli.py
@@ -3,7 +3,8 @@ from typing import Optional
 import click
 
 from scripts.client import TC
-from scripts.trigger_auto_tests.main import main
+from scripts.utils.models import AutoTestsInfo
+from scripts.utils.trigger_helpers import trigger_tests
 
 
 @click.group()
@@ -15,10 +16,16 @@ def cli():
     "trigger-auto-tests",
     help="Trigger Automated Tests on TeamCity for specified Shells and changed package",
 )
-@click.option("--tc-user", required=True, help="TeamCity User")
-@click.option("--tc-password", required=True, help="TeamCity Password")
-def trigger_auto_tests(tc_user: str, tc_password: str):
-    main(tc_user, tc_password)
+@click.option("--tc-url", required=False, help="TeamCity URL")
+@click.option("--tc-user", required=False, help="TeamCity User")
+@click.option("--tc-password", required=False, help="TeamCity Password")
+def trigger_auto_tests(
+    tc_url: Optional[str], tc_user: Optional[str], tc_password: Optional[str]
+):
+    tc = TC(tc_url, tc_user, tc_password)
+    current_build = tc.get_current_build()
+    tests_info = AutoTestsInfo.from_current_build(current_build)
+    trigger_tests(tests_info, tc)
 
 
 @cli.command("get-commits-from-changes", help="Return commits from the VCS changes.")
